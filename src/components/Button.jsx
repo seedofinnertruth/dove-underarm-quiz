80281212bbf03e7fc23b6773a6fbf493b07b005e
rbidenx <seedofinnertruth@gmail.com>
2025-11-28 16:54:17 +0800

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
+import buttonBg from "../assets/images/background/button-pink-bg.svg";
+
 export default function Button({ children, onClick, className = "" }) {
    return (
       <button
          onClick={onClick}
          className={`relative px-8 lg:py-4 text-white font-medium text-base uppercase tracking-wide overflow-hidden cursor-pointer ${className}`}
          style={{
-            background: "url(/src/assets/images/background/button-pink-bg.svg)",
+            background: `url(${buttonBg})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             borderRadius: "50px",
